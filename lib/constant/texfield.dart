1b398f8cbdcf35e81d05e7ddf419a703e3bbcf10
EbrahimMShaaban <70702070+EbrahimMShaaban@users.noreply.github.com>
2022-05-20 15:36:40 +0200

    add Constant directory { - details_page.dart - loading.dart - texfield.dart - txtbutton.dart }

---
+++ lib/constant/texfield.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InputFieldRegist extends StatefulWidget {
+  final String hint;
+  final String label;
+  final bool scure;
+  final FormFieldValidator validator;
+  // final Function onSaved;
+  final Function (String)onChanged;
+  const InputFieldRegist(
+      {Key? key, required this.hint,
+        required this.label,
+        required this.scure,
+        required this.validator,
+        // required this.onSaved,
+        required this.onChanged
+      }) : super(key: key);
+
+  @override
+  _InputFieldRegistState createState() => _InputFieldRegistState();
+}
+
+class _InputFieldRegistState extends State<InputFieldRegist> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12.5),
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: TextFormField(
+          onChanged: widget.onChanged ,
+          obscureText: widget.scure,
+          validator: widget.validator,
+          // onSaved: widget.onSaved(),
+          decoration: InputDecoration(
+            enabledBorder:  UnderlineInputBorder(
+              borderSide:  BorderSide(color: Colors.purple, width: 2.5),
+            ),
+            focusedBorder:  UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.purple, width: 2.5),
+            ),
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            labelText: widget.label,
+            hintText: widget.hint,
+            labelStyle: GoogleFonts.tajawal(
+                textStyle: const  TextStyle(
+                    fontSize: 28, fontWeight: FontWeight.w600, color: Colors.grey, height: .2)),
+            hintStyle: GoogleFonts.tajawal(
+                textStyle: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blueGrey,
+                    height: 1.5)),
+          ),
+        ),
+      ),
+    );
+  }
+}
